91ec3ee026a45f25d5cf94295b55b0dcd9cb27d1
Liebranca <dantepaola25@gmail.com>
2022-01-13 10:13:08 -0300

    llkeys,genheds

--- ce.c
+++ ce.c
@@ -13,6 +13,7 @@
 // deps
 
   #include <stddef.h>
+  #include <stdlib.h>
   #include <string.h>
   #include <stdio.h>
 
@@ -28,39 +29,96 @@
   #include <wchar.h>
   #include "chartab.h"
 
+  #include <sys/ioctl.h>
+
 // ---   *   ---   *   ---
 // constants
-  static const int FNO_STDIN=0;
-  static const int KBD_SZ=16;
+  #define KBD_SZ 0x08
+  #define CEB_SZ 0x400
+
+// ---   *   ---   *   ---
+// global state
+
+  typedef struct {
+    struct termios restore;
+    struct winsize wsz;
+
+    char buff[CEB_SZ];
+    size_t buff_i;
+
+    struct {
+      int x;
+      int y;
+
+    } cursor;
+
+    struct {
+      uint64_t l;
+      uint64_t r;
+
+    } kbd;
+
+  } STRUCT_CE;static STRUCT_CE CE={0};
+
+// ---   *   ---   *   ---
+// fwd decls
+  void iclose(void);
+
+// ---   *   ---   *   ---
+// buffer utils
+
+// manual cat
+void badd(char* src) {
+  strncpy(
+    CE.buff+CE.buff_i,
+    src,
+
+    CEB_SZ-CE.buff_i
+
+  );CE.buff_i+=strlen(src);
+  CE.buff_i&=CEB_SZ-1;
+
+// buffer wipe
+};void bcl(void) {memset(CE.buff,0,CEB_SZ);};
 
 // ---   *   ---   *   ---
 // term utils
 
-// open stdin for real-time user input
+// open stdin raw
 int iopen(void) {
 
-  if(!isatty(FNO_STDIN)) {
+  if(!isatty(STDIN_FILENO)) {
     printf("STDIN is not a tty\n");
     return 1;
 
-  };struct termios term;
-  tcgetattr(FNO_STDIN,&term);
+  };atexit(iclose);
 
-  term.c_lflag&=~(ICANON|ECHO);
-  tcsetattr(FNO_STDIN,TCSANOW,&term);
-  fcntl(FNO_STDIN,F_SETFL,O_NONBLOCK);
+  // put tty in raw mode
+  struct termios term={0};
+  tcgetattr(STDIN_FILENO,&CE.restore);
+
+  term.c_cc[VTIME]=2;
+  term.c_cc[VMIN]=0;
+
+  term.c_cflag|=(CS8);
+  term.c_oflag&=~(OPOST);
+  term.c_iflag&=~(BRKINT|INPCK|ISTRIP|IXON|ICRNL);
+  term.c_lflag&=~(ICANON|ECHO|IEXTEN|ISIG);
+  tcsetattr(STDIN_FILENO,TCSAFLUSH,&term);
+
+  // get screen size
+  ioctl(STDOUT_FILENO,TIOCGWINSZ,&CE.wsz);
+
+  // clear screen and reposition
+  badd("\e[?25l\e[2J\e[H");
+  CE.cursor.x=0;CE.cursor.y=0;
 
   return 0;
 
 // ^the undo for it
 };void iclose(void) {
-
-  struct termios term;
-  tcgetattr(FNO_STDIN,&term);
-
-  term.c_lflag|=(ICANON|ECHO);
-  tcsetattr(FNO_STDIN,TCSANOW,&term);
-  fcntl(FNO_STDIN,F_SETFL,~O_NONBLOCK);
+  tcsetattr(STDIN_FILENO,TCSAFLUSH,&CE.restore);
+  write(STDOUT_FILENO,"\e[2J\e[H",7);
 
 };
 
@@ -106,6 +164,31 @@ void tick(CLCK* c) {
 };
 
 // ---   *   ---   *   ---
+// keyhandler
+
+void kev(void) {
+  for(int x=0;x<8;x++) {
+
+    int b=x*2;
+    int y=(CE.kbd.l&(0b11<<b)) >> b;
+
+    int held=(y&0b10)!=0;int tap=y&0b01;
+
+    held=0b10<<b;
+    tap=0b01<<b;
+    int active=held|tap;
+
+    CE.kbd.l&=~active;
+    CE.kbd.l|=(y)*held;
+
+    break;
+
+  };
+};
+
+#include "keymap.h"
+
+// ---   *   ---   *   ---
 
 void main(void) {
 
@@ -130,39 +213,66 @@ void main(void) {
 
     8
 
+// ---   *   ---   *   ---
+
   // looparino
   );int x=1600;do {
 
-    fflush(stdout);
+    // refresh and wipe buffer
+    if(CE.buff_i) {
+      write(
+        STDOUT_FILENO,
+        CE.buff,
+        CE.buff_i
 
-    // print the clock and tick
-    printf("%lc\e[1D",clck.v[clck.vix]);
-
-    tick(&clck);
-
-    // capture this frames input
-    fgets(kbd,KBD_SZ,stdin);
-
-    // early exit
-    if(*kbd_ptr) { if(kbd[0]=='Q') {break;};
-      btt=*kbd_ptr;*(kbd_ptr)^=*(kbd_ptr);
-
+      );bcl();badd("\e[?25l");
     };
 
-    // bruteforce unlocked arrow keys for testing
+// ---   *   ---   *   ---
 
-           if(btt==0x415B1B) { printf("\e[1A");}
-      else if(btt==0x425B1B) { printf("\e[1B");}
-      else if(btt==0x435B1B) { printf("\e[1C");}
-      else if(btt==0x445B1B) { printf("\e[1D");};
+    // print the clock and tick
+    { char tmp[32];sprintf(tmp,
+        "\e[%u;1H\e[2K%lc",
 
-      btt^=btt;
+        CE.wsz.ws_row,
+        clck.v[clck.vix]
+
+      );badd(tmp);tick(&clck);
+    };
+
+// ---   *   ---   *   ---
+
+    // capture this frames input
+    read(STDIN_FILENO,kbd,KBD_SZ);
+
+    // exit or process input
+    if(*kbd_ptr) { if(kbd[0]=='Q') {break;};
+
+      // print the code for debug
+      char tmp[64];sprintf(tmp,
+        "\e[%u;1H%016"PRIX64"\e[K",
+        CE.wsz.ws_row-1,
+        *kbd_ptr
+
+      );badd(tmp);
+
+      keychk(*kbd_ptr);*kbd_ptr^=*kbd_ptr;
+
+    };kev();
+
+// ---   *   ---   *   ---
+
+    // move cursor to pos and unhide
+    { char tmp[64];sprintf(tmp,
+        "\e[%u;%uH\e[?25h",
+        CE.cursor.y+1,
+        CE.cursor.x+1
+
+      );badd(tmp);
+    };
 
   } while(x--);
-
-  // cleanup
-  printf("\n\n");
-  iclose();return;
+  return;
 
 };
 
