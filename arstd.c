ceef853cf29b3766431e0babbf470fc7e44e0579
Liebranca <dantepaola25@gmail.com>
2022-01-19 15:58:02 -0300

    Honest to god forgot the add switch

---
+++ arstd.c
@@ -0,0 +1,192 @@
+// ---   *   ---   *   ---
+// ARSTD
+// common routines
+//
+// LIBRE SOFTWARE
+// Licensed under GNU GPL3
+// be a bro and inherit
+//
+// CONTRIBUTORS
+// lyeb,
+// ---   *   ---   *   ---
+
+#include "arstd.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include <fcntl.h>
+#include <limits.h>
+#include <unistd.h>
+
+#include <errno.h>
+
+#include <sys/wait.h>
+
+// ---   *   ---   *   ---
+
+static char** PATH;
+static const size_t PATH_SZ=16;
+
+// build strarr from search path
+void getpath(void) {
+
+  PATH=malloc(PATH_SZ*sizeof(char*));
+  memset(PATH,0,PATH_SZ*sizeof(char*));
+
+  // get current directory
+  { static char cd[PATH_MAX+1];
+    getcwd(cd,PATH_MAX);
+
+    // ensure last char is /
+    int len=strlen(cd);
+    cd[len]='/';cd[len+1]=0x00;
+
+    PATH[0]=malloc((len+2)*sizeof(char));
+    strcpy(PATH[0],cd);
+
+  };
+
+// ---   *   ---   *   ---
+
+  // get $ARPATH
+  { char* arpath=getenv("ARPATH");
+    int len=strlen(arpath);
+
+    // copy
+    PATH[1]=malloc((len+2)*sizeof(char));
+    strcpy(PATH[1],arpath);
+
+    // ensure last char is /
+    PATH[1][len]='/';PATH[1][len+1]=0x00;
+
+  };
+
+// ---   *   ---   *   ---
+
+  // get $PATH
+  { char* envpath=getenv("PATH");
+    char* token=strtok(envpath,":");
+
+    int x=2;
+
+    // break up $PATH
+    while(token && x<PATH_SZ) {
+      int len=strlen(token);
+      PATH[x]=malloc((len+2)*sizeof(char));
+
+      // ensure last char is /
+      strcpy(PATH[x],token);
+      PATH[x][len]='/';PATH[x][len+1]=0x00;
+
+      // go to next
+      token=strtok(NULL,":");x++;
+
+    };
+  };
+
+// ---   *   ---   *   ---
+
+// ^free strarr
+};void freepath(void) {
+  for(int x=0;x<PATH_SZ;x++) {
+    if(PATH[x]) {free(PATH[x]);};
+
+  };free(PATH);
+
+// ---   *   ---   *   ---
+
+// search name in path
+};char* exsearch(char* ex_name) {
+
+  getpath();
+
+  // reserve some mem
+  static char ex_path[PATH_MAX+1];
+  int ex_err=1;
+
+  // look for ex_name
+  { char** p=PATH+0;
+    while(p) {
+
+    // cat
+    strcpy(ex_path,*p);
+    strcpy(ex_path+strlen(ex_path),ex_name);
+
+    // validate
+    ex_err=access(ex_path,X_OK);
+    if(!ex_err) {
+      break;
+
+    };p++;
+
+    // not found?
+    };if(ex_err) {return NULL;};
+
+  // found
+  };freepath();
+  return ex_path;
+
+};
+
+// ---   *   ---   *   ---
+
+char* ex(int argc,char** argv) {
+
+  int fd[2];pipe(fd);
+  pid_t pid=fork();
+
+  // child
+  if(!pid) {
+
+    dup2(fd[1],STDOUT_FILENO);
+    close(fd[0]);
+    close(fd[1]);
+
+    // find executable
+    char* ex_name=argv[0];
+    char* ex_path=exsearch(ex_name);
+    if(!ex_path) {
+      fprintf(stderr,"%s not found!\n",ex_name);
+      exit(-1);
+    };
+
+    // make strarr for args
+    char** ex_argv=malloc((1+argc)*sizeof(char*));
+    ex_argv[0]=ex_path;
+
+    // copy args
+    for(int x=1;x<argc;x++) {
+      ex_argv[x]=malloc(
+        strlen(argv[x])*sizeof(char)
+
+      );strcpy(ex_argv[x],argv[x]);
+    };ex_argv[argc]=NULL;
+    execv(ex_argv[0],ex_argv);
+
+    // you should never see this
+    perror("execv");
+    exit(-1);
+
+// ---   *   ---   *   ---
+
+  // parent
+  };close(fd[1]);
+
+  static char buff[0x1000];
+
+  int fc=fcntl(fd[0],F_GETFL,0);
+  fcntl(fd[0],F_SETFL,~FNONBLOCK);
+  ssize_t rb=read(fd[0],buff,sizeof(buff));
+
+  fcntl(fd[0],F_SETFL,fc);
+
+  close(fd[0]);wait(0);
+
+  return buff;
+
+};
+
+// ---   *   ---   *   ---
---
+++ arstd.h
@@ -0,0 +1,19 @@
+#ifndef __ARSTD_H__
+#define __ARSTD_H__
+
+#ifdef ___cplusplus
+extern "C" {
+#endif
+
+// ---   *   ---   *   ---
+
+// fork, call process and capture output
+char* ex(int argc,char** argv);
+
+// ---   *   ---   *   ---
+
+#ifdef ___cplusplus
+}
+#endif
+
+#endif // __ARSTD_H__
